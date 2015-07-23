1a38918fc2491092ed9f19c4b3c60cfef2a27ca1
Tamil Ramasamy <ramast1@labcorp.com>
2015-07-23 02:43:41 -0400

    half cooked ssn mask

--- src/form-ssn/index.js
+++ src/form-ssn/index.js
@@ -11,23 +11,19 @@ app.controller('MainCtrl', function ($scope) {
     };
 });
 
-app.directive('ssnFormatter', function () {
+app.directive('ssnInputText', function () {
 
 
     return {
 
-        restrict: 'A',
+        restrict: 'E',
         require: 'ngModel',
         scope: true,
+        template: '<input name="ssn" ssn-formatter type="text" ng-model="masked"/>',
+
         link: function (scope, element, attrs, ngModel) {
 
-
-
-            //format text going to user (model to view)
-            ngModel.$formatters.push(function (value) {
-
-                console.log('ssn formatter : ', value);
-                if(!value) return;
+            function getMasked(value) {
 
                 var last4 = '';
 
@@ -35,20 +31,59 @@ app.directive('ssnFormatter', function () {
                     var len = value.length;
                     last4 = value.substring(len - 4, len);
                 }
+                return '*****' + last4;
+            }
 
-                scope.origValue = value;
+            //format text going to user (model to view)
+            ngModel.$formatters.push(function (value) {
 
-                return   '***-**-' + last4;
+                console.log('ssn formatter : ', value);
+                if(!value) return;
+
+                return  {
+                    masked : getMasked(value),
+                    original: value
+                }
 
             });
 
+            ngModel.$render = function() {
+                console.log('render called ');
+                scope.masked = ngModel.$viewValue.masked;
+                scope.original  = ngModel.$viewValue.original;
+            };
 
+            scope.$watch('masked', function() {
+                console.log('watch called');
+                ngModel.$setViewValue({ masked: scope.masked, original: scope.original });
+            });
+
+           /* element.find('input').on('change', function() {
+
+            });
+            */
             //format text going to model (view to model)
-            ngModel.$parsers.push(function (value) {
+            ngModel.$parsers.push(function (viewValue) {
 
-                console.log('ssn parsers : ', value);
 
-                return value;
+                /*if(viewValue.masked && viewValue.original) {
+                    if(getMasked(viewValue.original) != viewValue.masked) {
+                        scope.masked = '';
+                        scope.original = '';
+                    }
+                } else {
+                    scope.original = viewValue.masked;
+                }
+                */
+                console.log('ssn parsers : ', viewValue);
+                var masked = String(scope.masked);
+                var newval = masked.replace(/[^0-9]+/g, '');
+
+                scope.original = newval;
+                scope.masked = getMasked(newval);
+                console.log('ssn parsers : ', viewValue);
+
+                return newval;
             });
 
 
